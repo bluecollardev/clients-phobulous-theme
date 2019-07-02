3722c355a2bdaef58b414fdd7f38516466a31c8e
Lucas <tech@firebrandwebsolutions.com>
2019-07-02 11:15:08 -0700

    Replace IP with domain.

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ var TEMPLATE_BUILD_DIR = __dirname + '/' + config.templateBuildDirectory; // Tem
 var TEMPLATE_SOURCE_DIR = __dirname + '/' + config.templateSourceDirectory; // Templates refer to Quick Commerce JSX templates
 
 // TODO: Env vars
-//var domainName = 'phobulous.ca';
-var domainName = '68.183.205.157';
+var domainName = 'phobulous.ca';
+//var domainName = '68.183.205.157';
 var wwwPrefix = (domainName === 'localhost' || /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/.test(domainName)) ? '' : 'www.';
 var baseUri = 'http://' + wwwPrefix + domainName + '/';
 
